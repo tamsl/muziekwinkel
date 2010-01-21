aeb75d70ccdf1911916bebe8529891fc0974e659
Hugo Arts <hugo.yoshi@gmail.com>
2010-01-21 12:25:07 +0100

    type colom toegevoegd, zodat niet alle klanten medewerkers zijn

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100119141841) do
+ActiveRecord::Schema.define(:version => 20100121111303) do
 
   create_table "aankoops", :force => true do |t|
     t.integer  "product_id"
@@ -26,6 +26,26 @@ ActiveRecord::Schema.define(:version => 20100119141841) do
     t.integer  "klant_id"
     t.text     "text"
     t.integer  "rating"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "klants", :force => true do |t|
+    t.string   "voornaam"
+    t.string   "achternaam"
+    t.boolean  "geslacht"
+    t.date     "geboortedatum"
+    t.string   "email"
+    t.string   "creditcard"
+    t.string   "postcode"
+    t.string   "huisnummer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "medewerkers", :force => true do |t|
+    t.string   "password"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -66,6 +86,7 @@ ActiveRecord::Schema.define(:version => 20100119141841) do
     t.string   "huisnummer"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "type"
   end
 
 end
